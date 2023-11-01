d49f8eba6e319e9821989cf182a8ecf6cc5f4554
Pavel Karyapkin <karyapkin@gmail.com>
2023-11-02 00:38:26 +0600

    fix keyboards

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const bot = require('./lib/bot');
-const actions = require('./lib/actions');
 const handlers = require('./lib/handlers');
 
-for (const trigger in actions) {
-	bot.action(trigger, actions[trigger]);
+for (const trigger in handlers) {
+	bot.action(trigger, handlers[trigger]);
 }
 
 bot.use(async (ctx) => {
 	if (ctx.message) {
-		console.log(ctx.message);
+		console.log('message', ctx.message);
 		const userMessage = ctx.message.text;
 		if (Object.hasOwn(handlers, userMessage)) {
 			handlers[userMessage](ctx);
 		}
+	} else if (ctx.update) {
+		console.log('update', ctx.update);
+		const userMessage = ctx.update.callback_query.data;
+		if (Object.hasOwn(handlers, userMessage)) {
+			handlers[userMessage](ctx);
+		}
+
 	}
 });
 
